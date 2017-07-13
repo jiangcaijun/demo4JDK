584d6d7fb4b8c9f75b24652a569401a54c8602d3
jiangcaijun <caijun.jiang@dbappsecurity.com.cn>
2017-07-13 11:36:57 +0800

    手写实现HashMap

---
+++ src/test/java/demo4JDK/CustomMap.java
@@ -0,0 +1,20 @@
+package demo4JDK;
+
+/**
+ * Created by jiangcaijun on 2017/6/8.
+ * 定义一个map的接口
+ */
+public interface CustomMap<K,V> {
+    V put(K key, V value);
+
+    V get(K key);
+
+    int size();
+
+    //定义一个内部接口
+    //可以根据Entry对象拿到这个对象的key和value
+    interface Entry<K,V>{
+        K getKey();
+        V getValue();
+    }
+}
